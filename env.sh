36d427d9eaa46c09210aef3e75576fccb660438e
Jan Max Meyer <jmm@phorward.de>
2017-11-06 22:24:24 +0100

    Many minor changes and a new "free software" part. Updated covers.

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 UNICC_DIR="/home/neo/Phorward/src/unicc"
 export PATH="$UNICC_DIR:$PATH"
-export UNICC_TPLDIR=$UNICC_DIR/tlt
+export UNICC_TPLDIR=$UNICC_DIR/templates
 
 HTMLDOC_DATA=/usr/local/share/htmldoc
 export HTMLDOC_DATA
